934321b614a66b99823fe386d62f92e325875996
Surya Manne <suryamanne@Suryas-MacBook-Air.local>
2025-03-29 01:06:50 +0530

    chore: added bananajs packge

---
+++ packages/bananajs/README.md
@@ -0,0 +1,7 @@
+# bananajs
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build bananajs` to build the library.
---
+++ packages/bananajs/src/index.ts
@@ -0,0 +1,6 @@
+import { BananaApp } from './lib/Core/App';
+export default BananaApp;
+
+export * from './lib/Core/App';
+export * from './lib/Router/Route.decorator';
+export * from './lib/Router/Controller.decorator';
